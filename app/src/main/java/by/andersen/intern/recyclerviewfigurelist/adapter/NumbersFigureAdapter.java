6333e07ca3e88338f8955749d929eb4d6d563d9f
artofpaganini@gmail.com <Grozbeckert2015>
2020-09-11 20:07:45 +0300

    numbersFigureView.setOnClickListener(new View.OnClickListener() в ViewHoldere  не работает с фрагментом, с тоаст  работает

--- app/src/main/java/by/andersen/intern/recyclerviewfigurelist/adapter/NumbersFigureAdapter.java
+++ app/src/main/java/by/andersen/intern/recyclerviewfigurelist/adapter/NumbersFigureAdapter.java
@@ -1,28 +1,31 @@
 package by.andersen.intern.recyclerviewfigurelist.adapter;
 
+
 import android.content.Context;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import by.andersen.intern.recyclerviewfigurelist.R;
+import by.andersen.intern.recyclerviewfigurelist.activity.MainActivity;
 import by.andersen.intern.recyclerviewfigurelist.dialog.AlertDialogFragment;
+
 
 public class NumbersFigureAdapter extends RecyclerView.Adapter<NumbersFigureAdapter.NumberViewHolder> {
 
     Context context;
     String[] numberStringArray;
     int[] imagesArray;
+
 
 
     public NumbersFigureAdapter(Context context, String[] numberStringArray, int[] imagesArray) {
@@ -34,7 +37,7 @@ public class NumbersFigureAdapter extends RecyclerView.Adapter<NumbersFigureAdap
 
     @NonNull
     @Override
-    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public NumberViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, int viewType) {
         int layoutIdForListItem = R.layout.number_list_item;
         LayoutInflater layoutInflater = LayoutInflater.from(context);
 
@@ -42,22 +45,14 @@ public class NumbersFigureAdapter extends RecyclerView.Adapter<NumbersFigureAdap
         View view = layoutInflater.inflate(layoutIdForListItem, parent, false);
         NumberViewHolder viewHolder = new NumberViewHolder(view);
 
+
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NumberViewHolder holder, final int position) {
         holder.numbersFigureView.setText(numberStringArray[position]);
         holder.imageFigureView.setImageResource(imagesArray[position]);
-
-//        holder.constraintLayout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent =  new Intent(context,AlertDialogFragment.class);
-//                intent.putExtra("image_url",imagesArray); //  тут может быть не верно
-//                context.startActivity(intent);
-//            }
-//        });
 
     }
 
@@ -69,9 +64,10 @@ public class NumbersFigureAdapter extends RecyclerView.Adapter<NumbersFigureAdap
 
     class NumberViewHolder extends RecyclerView.ViewHolder {
 
+        ConstraintLayout constraintLayout;
         TextView numbersFigureView;
         ImageView imageFigureView;
-        ConstraintLayout constraintLayout;
+
 
         // оборачивает элемент списка,
         public NumberViewHolder(@NonNull View itemView) {
@@ -80,83 +76,19 @@ public class NumbersFigureAdapter extends RecyclerView.Adapter<NumbersFigureAdap
             numbersFigureView = itemView.findViewById(R.id.tv_number_image);
             imageFigureView = itemView.findViewById(R.id.iv_figure);
 
+            numbersFigureView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context,"123",Toast.LENGTH_SHORT).show();
+                    AlertDialogFragment alertDialogFragment = new AlertDialogFragment ();
+                    ((MainActivity)context).getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.rv_figure_list, alertDialogFragment,"AlertDialogFragment").addToBackStack(null).commit();
 
-        }
+                }
+            });
 
-        void  openDialog(){
-            AlertDialogFragment alertDialogFragment = new AlertDialogFragment();
-            FragmentManager fragmentManager= ((AppCompatActivity)context).getSupportFragmentManager();
 
-        }
-
-        //возможно тут  нужно  удет делать цикл или вставлять  массив  строк
-        void bind(int listIndex) {
-            numbersFigureView.setText(String.valueOf(listIndex));
         }
     }
 }
-//    private int numberFigureListSize;
-//    Context context;
-//
-//
-//    public NumbersFigureAdapter(int numberFigureListSize) {
-//        this.numberFigureListSize = numberFigureListSize;
-//    }
-//
-//    //начальное создание  Н-е кол-во  вьюхолдеров
-//    @NonNull
-//    @Override
-//    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//      context = parent.getContext();
-//
-//        int layoutIdForListItem = R.layout.number_list_item;
-//
-//
-//
-//        LayoutInflater layoutInflater = LayoutInflater.from(context);
-//
-//        //  тут  мы получили  наше предсталвение  из  xml  в java
-//        View view = layoutInflater.inflate(layoutIdForListItem, parent, false);
-//
-//        //оборачивает во вьюхолдер, это позволит переиспользовать
-//        NumberViewHolder viewHolder = new NumberViewHolder(view);
-//
-//        return viewHolder;
-//    }
-//
-//    //вместо новых  вьюхолдеров ,  задаем новые значения   созданным ранее вьюхолдерам
-//    @Override
-//    public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
-//        holder.bind(position);
-//
-//    }
-//
-//    //общее кол-во  вьюхолдеров
-//    @Override
-//    public int getItemCount() {
-//        return numberFigureListSize;
-//    }
-//
-//    //одна ячейка РВ
-//    class NumberViewHolder extends RecyclerView.ViewHolder {
-//
-//        TextView listNumbersFigureView;
-//        ImageView imageFigureView;
-//
-//
-//        // оборачивает элемент списка,
-//        public NumberViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//
-//            listNumbersFigureView = itemView.findViewById(R.id.tv_number_image);
-//            imageFigureView = itemView.findViewById(R.id.iv_figure);
-//
-//
-//        }
-//
-//        //возможно тут  нужно  удет делать цикл или вставлять  массив  строк
-//void bind(int listIndex) {
-//    listNumbersFigureView.setText(String.valueOf(listIndex));
-//}
-//    }
-
--- app/src/main/java/by/andersen/intern/recyclerviewfigurelist/dialog/AlertDialogFragment.java
+++ app/src/main/java/by/andersen/intern/recyclerviewfigurelist/dialog/AlertDialogFragment.java
@@ -1,8 +1,8 @@
 package by.andersen.intern.recyclerviewfigurelist.dialog;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +12,12 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.appcompat.app.AlertDialog;
+
 import androidx.fragment.app.DialogFragment;
 
 import by.andersen.intern.recyclerviewfigurelist.R;
+
 
 
 public class AlertDialogFragment extends DialogFragment {
@@ -26,19 +28,23 @@ public class AlertDialogFragment extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-
         View view = inflater.inflate(R.layout.alert_dialog_fragment, container, false);
         buttonOk = view.findViewById(R.id.buttonOK);
         inputTextView = view.findViewById(R.id.text_title);
 
-        buttonOk.setOnClickListener(new View.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.title)
+                .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                getDialog().dismiss();
+            public void onClick(DialogInterface dialogInterface, int i) {
+               dismiss();
             }
         });
+
+
         return view;
     }
+
+
 
 }
